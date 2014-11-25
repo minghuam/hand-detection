1f2a604f4408eeea5122695fa49653bd84f16257
minghuam <rexma08@gmail.com>
2014-11-25 15:31:29 -0500

    swap files

--- test.cpp
+++ test.cpp
@@ -11,17 +11,17 @@ int main(int argc, char **argv){
 
 	std::string mask_path = "./images/mask";
 	std::string rgb_path = "./images/rgb";
-	std::string classifier_path = "./models/classifiers"
-	std::string gfeat_path = "./models/histogram"
+	std::string classifier_path = "./models/classifiers";
+	std::string gfeat_path = "./models/histogram";
 	
-	//hd.train_and_save(rgb_files, mask_files, "rvl", classifier_path, gfeat_path);
+	hd.train_and_save(rgb_files, mask_files, "rvl", classifier_path, gfeat_path);
 
-	std::vector<std::string> mask_files = list_dir(mask_path, ".jpg");
-	std::vector<std::string> rgb_files = list_dir(rgb_path, ".jpg");
+	//std::vector<std::string> mask_files = list_dir(mask_path, ".jpg");
+	//std::vector<std::string> rgb_files = list_dir(rgb_path, ".jpg");
 	
-	hd.load_models(model_files, gfeat_files);
+	//hd.load_models(model_files, gfeat_files);
 
-	
+
 
 }
 
